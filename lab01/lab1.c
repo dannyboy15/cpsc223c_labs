7c2443dee2411a67ae0f9778d8be20b890df0c1c
Daniel Bravo <bravod@csu.fullerton.edu>
2018-02-28 15:05:20 -0800

    Add lab 1

---
+++ lab01/lab1.c
@@ -0,0 +1,59 @@
+// Daniel Bravo
+
+// lab1.cpp - convert this code to C11
+
+/*
+ *  This code should compile cleanly with the following options:
+ *
+ 
+    C:   -std=c11   -g -Og -Wall -Wextra -Wpedantic
+
+    C++: -std=c++11 -g -Og -Wall -Wextra -Wpedantic
+
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+void bumpInt(int* i, int* amt) {
+    if(amt) {
+        *i += *amt;
+    }
+    else {
+        *i += 1;
+    }
+}
+
+
+void bumpStr(char* s, char* c) {
+    if(c) {
+        strcat(s, c);
+    }
+    else {
+        strcat(s, "o");
+    }
+}
+
+
+int main()
+{
+    int i = 1;
+
+    printf("%d\n", i);
+    bumpInt(&i, NULL);
+    printf("%d\n", i);
+    int j = 2;
+    bumpInt(&i, &j);
+    printf("%d\n", i);
+
+    char s[4] = "foo";
+
+    printf("%s\n", s);
+    bumpStr(s, NULL);
+    printf("%s\n", s);
+    char a[2] = "x";
+    bumpStr(s, a);
+    printf("%s\n", s);
+
+    return 0;
+}
