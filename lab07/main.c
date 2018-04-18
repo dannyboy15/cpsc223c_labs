9b4f08d28decb08152eef1e59ba99ba909902aec
Daniel Bravo <bravoappdev@gmail.com>
2018-04-17 18:29:48 -0700

    Implement main.c

--- lab07/main.c
+++ lab07/main.c
@@ -1,3 +1,9 @@
+/* Daniel Bravo
+ * 2018-04-04
+ * CPSC 223C-01/02
+ * Lab 7
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,12 +11,41 @@
 #include "commands.h"
 #include "faculty.h"
 
+int cmd_cmp (const void * a, const void * b)
+{
+    return strcmp(a, ((struct command *)b)->name);
+}
+
+
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
+    if (argc < 3) {
+        printf("Usage: phonebook <command> <string>\n");
+        return EXIT_FAILURE;
+    }
 
-    // INSERT CODE HERE
+    struct command * cmd = (struct command *) bsearch(
+        argv[1],
+        commands,
+        NUM_COMMANDS,
+        sizeof(struct command),
+        cmd_cmp);
+
+    if (cmd) {
+        int i = cmd->function(argv[2]);
+
+        if (i >= 0) {
+            printf("%s\n%s\n%s\n%s\n",
+                faculty[i].name,
+                faculty[i].office,
+                faculty[i].phone,
+                faculty[i].email);
+        } else {
+            printf("No match.\n");
+        }
+    } else {
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
