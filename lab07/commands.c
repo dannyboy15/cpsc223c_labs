b5d7e56cda610e7904f0abf3b467d48d0debfe81
Daniel Bravo <bravoappdev@gmail.com>
2018-04-17 18:30:53 -0700

    Fix find functions to use qsort and bsearch

--- lab07/commands.c
+++ lab07/commands.c
@@ -1,3 +1,12 @@
+/* Daniel Bravo
+ * 2018-04-04
+ * CPSC 223C-01/02
+ * Lab 7
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "commands.h"
 #include "faculty.h"
 
@@ -8,41 +17,123 @@ struct command commands[NUM_COMMANDS] = {
     { "phone",  find_by_phone },
 };
 
+int name_cmp(const void * a, const void * b)
+{
+    return strcmp(((struct professor *)a)->name, ((struct professor *)b)->name);
+}
+
+int office_cmp(const void * a, const void * b)
+{
+    return strcmp(((struct professor *)a)->office, ((struct professor *)b)->office);
+}
+
+int phone_cmp(const void * a, const void * b)
+{
+    return strcmp(((struct professor *)a)->phone, ((struct professor *)b)->phone);
+}
+
+int email_cmp(const void * a, const void * b)
+{
+    return strcmp(((struct professor *)a)->email, ((struct professor *)b)->email);
+}
+
 int find_by_name(char *name)
 {
-    // REMOVE THE FOLLOWING LINE BEFORE ADDING YOUR CODE
-    (void) name;
+    if (strlen(name) ==  0) {
+        return -1;
+    }
 
-    // INSERT CODE HERE
-    
-    return -1;
+    qsort(faculty, NUM_FACULTY, sizeof(struct professor), name_cmp);
+
+    struct professor p;
+    strcpy(p.name, name);
+
+    struct professor * prof = (struct professor *) bsearch(
+        &p,
+        faculty,
+        NUM_FACULTY,
+        sizeof(struct professor),
+        name_cmp);
+
+    if (prof) {
+        return prof - faculty;
+    } else {
+        return -1;
+    }
 }
 
 int find_by_office(char *office)
 {
-    // REMOVE THE FOLLOWING LINE BEFORE ADDING YOUR CODE
-    (void) office;
+    if (strlen(office) ==  0) {
+        return -1;
+    }
 
-    // INSERT CODE HERE
+    qsort(faculty, NUM_FACULTY, sizeof(struct professor), office_cmp);
 
-    return -1;
+    struct professor p;
+    strcpy(p.office, office);
+
+    struct professor * prof = (struct professor *) bsearch(
+        &p,
+        faculty,
+        NUM_FACULTY,
+        sizeof(struct professor),
+        office_cmp);
+
+    if (prof) {
+        return prof - faculty;
+    } else {
+        return -1;
+    }
 }
 
 int find_by_phone(char *phone)
 {
-    (void) phone;
+    if (strlen(phone) ==  0) {
+        return -1;
+    }
 
-    // INSERT CODE HERE
+    qsort(faculty, NUM_FACULTY, sizeof(struct professor), phone_cmp);
 
-    return -1;
+    struct professor p;
+    strcpy(p.phone, phone);
+
+    struct professor * prof = (struct professor *) bsearch(
+        &p,
+        faculty,
+        NUM_FACULTY,
+        sizeof(struct professor),
+        phone_cmp);
+
+    if (prof) {
+        return prof - faculty;
+    } else {
+        return -1;
+    }
 }
 
 int find_by_email(char *email)
 {
-    (void) email;
+    if (strlen(email) ==  0) {
+        return -1;
+    }
 
-    // INSERT CODE HERE
-    
-    return -1;
+    qsort(faculty, NUM_FACULTY, sizeof(struct professor), email_cmp);
+
+    struct professor p;
+    strcpy(p.email, email);
+
+    struct professor * prof = (struct professor *) bsearch(
+        &p,
+        faculty,
+        NUM_FACULTY,
+        sizeof(struct professor),
+        email_cmp);
+
+    if (prof) {
+        return prof - faculty;
+    } else {
+        return -1;
+    }
 }
 
